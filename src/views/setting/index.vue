2113b57a67e5e80b0aa91b0fed12adec78d0a798
w17 <1050363049@qq.com>
2022-08-11 09:36:18 +0800

    公司设置

--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -1,7 +1,26 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>公司设置</h2>
+      <el-tabs v-model="activeName" @tab-click="handleClick">
+        <el-tab-pane label="角色管理" name="first">
+          <el-button type="primary">新建</el-button>
+          <el-table :data="tableData" style="width: 100%">
+            <el-table-column type="index" label="序号" width="180">
+            </el-table-column>
+            <el-table-column prop="name" label="姓名" width="180">
+            </el-table-column>
+            <el-table-column prop="address" label="操作">
+              <template>
+                <el-button type="success">分配权限</el-button>
+                <el-button type="primary">编辑</el-button>
+                <el-button type="danger">删除</el-button>
+              </template>
+            </el-table-column>
+          </el-table></el-tab-pane
+        >
+
+        <el-tab-pane label="公司信息" name="second">公司信息</el-tab-pane>
+      </el-tabs>
     </div>
   </div>
 </template>
@@ -9,12 +28,40 @@
 <script>
 export default {
   data() {
-    return {}
+    return {
+      activeName: 'first',
+      tableData: [
+        {
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄',
+        },
+        {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄',
+        },
+        {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄',
+        },
+        {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄',
+        },
+      ],
+    }
   },
 
   created() {},
 
-  methods: {}
+  methods: {
+    handleClick(tab, event) {
+      console.log(tab, event)
+    },
+  },
 }
 </script>
 
